b4d33eccb8815395c9ce5c04696342826f9f2087
Guillaume Erny <glerny@users.noreply.github.com>
2016-12-13 15:16:31 +0000

    Add files via upload

---
+++ List of Objects/AnalyzeThis.m
@@ -0,0 +1,228 @@
+classdef AnalyzeThis
+    
+    properties
+        %% Options for fileInfo
+        DataIn              = []
+        % First colum: axe; followiing traces (cn be more than 1 nut should
+        % be related
+        
+        BaselineMethod  	= struct('name', 'ArPLS', 'param1', 10E6)
+        % Possible methods:
+        %   'None'      No additional parameter
+        % 	'Constant'  No additional parameter
+        % 	'Linear'    No additional parameter
+        %   'ArPLS'     One additional paremater recommanded value 10E2-10E9
+        %               Detault method with 10E6 as paramters
+        
+        SmoothingMethod     = struct('name', 'None')
+        % Possible methods:
+        %   'None'      No additional parameter
+        
+        PeakPickingMethod 	= struct('name', 'LmMm', 'param1', 2)
+        % Possible methods:
+        %   'LmMm'      One additional parameter, integer from 0-9
+        
+        DeconvolutionMethod = struct('name', 'None')
+        % Possible methods:
+        %   'None'      No additional parameter
+        
+        Threshold = [];
+        
+    end
+    
+    properties (Dependent)
+        Baseline
+        PeakList
+        Deconvolution
+    end
+    
+    methods
+        function obj = AnalyzeThis(dataIn, varargin)
+            obj.DataIn = dataIn;
+            
+            if nargin  < 2
+                return
+            end
+            
+            input = @(x) find(strcmpi(varargin,x),1);
+            
+            tgtIx = input('baseline');
+            if ~isempty(tgtIx)
+                str2dec = varargin{tgtIx +1};
+                C =  regexp(str2dec, ':', 'split');
+                switch lower(C{1})
+                    case 'none'
+                        obj.BaselineMethod = struct('name', 'None');
+                        
+                    case 'constant'
+                        obj.BaselineMethod = struct('name', 'Constant');
+                        
+                    case 'linear'
+                        obj.BaselineMethod = struct('name', 'Linear');
+                        
+                    case 'arpls'
+                        if length(C) < 2
+                            C(2) = '10E6';
+                        end
+                        
+                        obj.BaselineMethod = struct(...
+                            'name', 'ArPLS',...
+                            'param1', str2double(C(2)) );
+                end
+            end
+            
+            tgtIx = input('peakpicking');
+            if ~isempty(tgtIx)
+                str2dec = varargin{tgtIx +1};
+                C =  regexp(str2dec, ':', 'split');
+                switch lower(C{1})
+                    case 'lmmm'
+                        if length(C) < 2
+                            C(2) = '2';
+                        end
+                        
+                        obj.PeakPickingMethod = struct(...
+                            'name', 'LmMm',...
+                            'param1', str2double(C(2)) );
+                end
+            end
+            
+            tgtIx = input('threshold');
+            if ~isempty(tgtIx)
+                obj.Threshold = varargin{tgtIx +1};
+            end
+        end
+        
+        function baseline = get.Baseline(obj)
+            
+            baseline.definition = obj.BaselineMethod;
+            baseline.noise(1) = NaN;
+            
+            switch obj.BaselineMethod.name
+                case 'ArPLS'
+                    [~, n] = size(obj.DataIn);
+                    results(:,1) = obj.DataIn(:,1);
+                    lambda = obj.BaselineMethod.param1;
+                    
+                    for ii = 2:n
+                        yori = obj.DataIn(:,ii);
+                        indNotZeros = yori ~= 0;
+                        [z, bslPts, ~] = doArPLS(yori(indNotZeros), lambda);
+                        results(indNotZeros, ii) = z;
+                        baseline.noise(ii) = 4*std(z(bslPts));
+                    end
+                    
+                case 'None'
+                    [~, n] = size(obj.DataIn);
+                    results = zeros(size(obj.DataIn));
+                    results(:,1) = obj.DataIn(:,1);
+                    
+                    for ii = 2:n
+                        yori = obj.DataIn(:,ii);
+                        baseline.noise(ii) = 4*std(yori);
+                    end
+            end
+            baseline.values = results;
+        end
+        
+        function peakList = get.PeakList(obj)
+            [~, n] = size(obj.DataIn);
+            peakList(n - 1) = struct();
+            for ii = 2:n;
+                if isempty(obj.Threshold)
+                    thrs = 3* obj.Baseline.noise(ii);
+                else
+                    thrs = obj.Threshold;
+                end
+                
+                yOri = obj.DataIn(:,ii) - obj.Baseline.values(:,ii);
+                
+                switch obj.PeakPickingMethod.name
+                    case 'LmMm'
+                        X = spread(yOri, obj.PeakPickingMethod.param1);
+                        
+                        locMax = find(X(:,((1+end)/2)) == max(X, [], 2) & max(X, [], 2) >= thrs);
+                        locMin =  find(X(:,((1+end)/2)) == min(X, [], 2));
+                        IdPair = zeros(length(locMax), 2);
+                        for jj = 1:length(locMax)
+                            IX = find(locMin < locMax(jj), 1, 'last');
+                            if isempty(IX), IX = 1; end
+                            IdPair(jj, 1) = locMin(IX);
+                            
+                            IX = find(locMin > locMax(jj), 1, 'first');
+                            if isempty(IX), IX = length(locMin); end
+                            IdPair(jj, 2) = locMin(IX);
+                            
+                        end
+                        
+                        [~, m, ~] = unique(IdPair(:,1));
+                        IdPair = IdPair(m, :);
+                        I2R = IdPair(:,2) - IdPair(:,1) < 2;
+                        IdPair(I2R, :) = [];
+                        
+                        FOM = zeros(length(IdPair(:,2)), 8);
+                        
+                        peakList.headings = {'1:PeakStart', '2:PeakEnd', ...
+                            '3:M0', '4:M1', '5:M2', '6:M3', ...
+                            '7:Imax', '8:@Imax'};
+                        for jj = 1:length(IdPair(:,2))
+                            xc = obj.DataIn(IdPair(jj,1): IdPair(jj,2), 1);
+                            yc = yOri(IdPair(jj,1): IdPair(jj,2));
+                            FOM(jj,1) = xc(1);
+                            FOM(jj,2) = xc(end);
+                            FOM(jj,3) = trapz(xc, yc);
+                            FOM(jj,4) = trapz(xc, xc.*yc)/FOM(jj,3);
+                            FOM(jj,5) = trapz(xc, (xc-FOM(jj,4)).^2.*yc)/FOM(jj,3);
+                            FOM(jj,6) = trapz(xc, (xc-FOM(jj,4)).^3.*yc)/FOM(jj,3);
+                            [FOM(jj,7), indAtMax] = max(yc);
+                            FOM(jj,8) = xc(indAtMax);
+                        end
+                        
+                       
+                end
+            end
+            peakList.data = FOM;      
+        end
+        
+        function plotAnalysis(obj)
+            [~, n] = size(obj.DataIn);
+            for ii = 2:n
+                figure('Name', 'Result AnalyzeThis');
+                hold on
+                lim2Plot(:,1) = ...
+                    [obj.PeakList.data(:,1); obj.PeakList.data(:,2)];
+                for jj = 1:length(lim2Plot(:,1))
+                    lim2Plot(jj,2) = ...
+                        obj.DataIn(obj.DataIn(:,1) == lim2Plot(jj,1),2);
+                    lim2Plot(jj,3) = ...
+                       obj.Baseline.values(obj.DataIn(:,1) == lim2Plot(jj,1),2);
+                end
+                
+                plot(obj.DataIn(:,1), obj.DataIn(:,2));
+                plot(obj.Baseline.values(:,1), obj.Baseline.values(:,2), 'r')
+                stem(lim2Plot(:,1), lim2Plot(:,2), 'k', 'Marker', 'none')
+                stem(lim2Plot(:,1), lim2Plot(:,3), 'w', 'Marker', 'none')
+                
+                fprintf('peakNbr \t%s \t%s \t%s \t%s \t%s \t%s \t%s \t%s', obj.PeakList.headings{:});
+                fprintf('\n');
+                for jj = 1:length(obj.PeakList.data(:,1))
+                    fprintf('\n');
+                    fprintf('#:%i \t%.4g \t%.4g \t%.4g \t%.4g \t%.4g \t%.4g \t%.4g \t%.4g', [jj, obj.PeakList.data(jj,:)]);
+                    
+                end
+                fprintf('\n');
+                
+                
+                hold off
+                
+            end
+        end
+        
+    end
+    
+    
+    
+    
+    
+end
+
---
+++ List of Objects/Options4bslCor.m
@@ -0,0 +1,285 @@
+classdef Options4bslCor
+    
+    properties
+        Path2Fin     = ''
+        Link2Log
+        Link2Noise  = ''
+        Link2SelPrf = ''
+        Link2RemPrf = ''
+        TimeAxe
+        MzAxe
+        FIS
+        IndMin
+        IndMax
+        Precision
+        Profiles
+        ProfilesTitle = {'Time', 'Noise', 'TIP1', 'BPP1', 'TIP2', 'BPP2'}
+        Fmin
+        Fmax
+        NoiseEstim
+        BaselineChoice
+    end
+    
+    methods
+        function obj = Options4bslCor(DtsIn)
+            if strcmp(DtsIn.Format, 'MS profile')
+                
+            else
+                error('Incorrect Format. The dataset should be made of MS profile scans');
+            end
+            
+            obj.Path2Fin    = DtsIn.Path2Fin;
+            obj.Link2Log    = DtsIn.Log;
+            obj.TimeAxe     = DtsIn.TimeAxe;
+            obj.MzAxe       = DtsIn.MzAxe;
+            obj.FIS         = DtsIn.FIS.Data(:,2)/...
+                length(DtsIn.TimeAxe.Data)*100;
+            obj.Link2SelPrf = tempname;
+            obj.Link2RemPrf = tempname;
+            obj.Precision   = 'uint32';
+            
+            figure
+            [N,edges] = histcounts(obj.FIS ,1:1:100);
+            bar(edges, [0, N]);
+            axis([0 100 0 inf])
+            
+            hPlot = gcf;
+            title('Frequency profile - Lower part:noise; higher part : bckg correction');
+            
+            h = msgbox('Select the low-end value in the frequency profile', 'Noise','modal');
+            uiwait(h)
+            figure(hPlot);
+            [fmin, ~] = ginput(1);
+            if isempty(fmin)
+                return
+            end
+            fmin = inputdlg('Confirm the value', 'Noise', 1, {num2str(fmin)});
+            
+            h = msgbox('Select the high-end value in the frequency profile', 'Correct','modal');
+            uiwait(h)
+            figure(hPlot);
+            [fmax, ~] = ginput(1);
+            if isempty(fmax)
+                return
+            end
+            fmax = inputdlg('Confirm the value', 'Correct', 1, {num2str(fmax)});
+            
+            obj.IndMin = obj.FIS <= str2double(fmin{1});
+            obj.IndMax = obj.FIS >= str2double(fmax{1});
+            obj.Fmin = str2double(fmin{1});
+            obj.Fmax = str2double(fmax{1});
+            
+            h = waitbar(0, 'Generating matrix of profiles');
+            fidWSelPrf = fopen(obj.Link2SelPrf, 'ab');
+            fidWRemPrf = fopen(obj.Link2RemPrf , 'ab');
+            
+            profile(:,1) = DtsIn.TimeAxe.Data;
+            data(:,1) = obj.MzAxe.Data;
+            for ii = 1:length(profile(:,1))
+                waitbar(ii/length(profile(:,1)))
+                
+                data(:,2) = 0;
+                MS = DtsIn.ListOfScans{ii}.Data;
+                cst = DtsIn.ListOfScans{ii}.Variables;
+                rdg = DtsIn.RndFactor;
+                axe = MS(:,1)/(1-cst);
+                [tf, loc] = ismember(round(axe*10^rdg)/10^rdg, ...
+                    round(data(:,1)*10^rdg)/10^rdg);
+                
+                indZeros = find(tf == 0);
+                if ~isempty(indZeros)
+                    [~, locc] = ismember(ceil(axe*10^rdg)/10^rdg, ...
+                        ceil(data(:,1)*10^rdg)/10^rdg);
+                    loc(indZeros) = locc(indZeros);
+                end
+                data(loc,2) =  MS(:,2);
+                
+                
+                fwrite(fidWSelPrf, data(obj.IndMax, 2), obj.Precision);
+                profile(ii,2) = max( data(obj.IndMin, 2));
+                profile(ii,3) = sum( data(obj.IndMax, 2));
+                profile(ii,4) = max( data(obj.IndMax, 2));
+                fwrite(fidWRemPrf, data(~obj.IndMax, 2), obj.Precision);
+                profile(ii,5) = sum(data(~obj.IndMax, 2));
+                profile(ii,6) = max( data(~obj.IndMax, 2));
+                
+                
+            end
+            close(h)
+            fclose(fidWSelPrf);
+            fclose(fidWRemPrf);
+            obj.Profiles = profile;
+            
+            figure('Name', 'Selected Profiles for Baseline Corrections')
+            subplot(5, 1, 1)
+            plot(profile(:,1), profile(:,2));
+            obj.NoiseEstim = round(mean(profile(:,2)));
+            title(['Estimated noise: ', num2str(obj.NoiseEstim)])
+            ylabel([DtsIn.ZLabel, ' / ', DtsIn.ZUnit]);
+            
+            subplot(5, 1, [2 3])
+            plot(profile(:,1), profile(:,4));
+            title(['Selected profiles for correction (max plot): ', num2str(sum(obj.IndMax))])
+            ylabel([DtsIn.ZLabel, ' / ',  DtsIn.ZUnit]);
+            
+            subplot(5, 1, [4 5])
+            plot(profile(:,1), profile(:,6));
+            title(['Non-selected profiles (max plot): ', num2str(sum(~obj.IndMax))])
+            xlabel([DtsIn.XLabel, ' / ', DtsIn.XUnit]);
+            ylabel([DtsIn.ZLabel, ' / ', DtsIn.ZUnit]);
+            
+        end
+        
+        function obj = setBslParameters(obj)
+            % Figure and axes
+            f = figure('Units', 'normalized', 'Visible', 'off', 'CloseRequestFcn', @done);
+            ax1 = axes('Units', 'normalized', 'Position', [0.03 0.6 0.67, 0.344]);
+            ax2 = axes('Units', 'normalized', 'Position', [0.03 0.08 0.67, 0.344]);
+            edNoise = uicontrol('Style', 'edit',...
+                'String', num2str(obj.NoiseEstim),...
+                'Units', 'normalized',...
+                'Callback', @changeEdit ,...
+                'HorizontalAlignment', 'left',...
+                'FontWeight', 'bold',...
+                'Position', [0.6 0.38 0.02 0.035]);
+            
+            uicontrol('Style', 'text',...
+                'String', 'Noise: ',...
+                'Units', 'normalized',...
+                'HorizontalAlignment', 'left',...
+                'FontWeight', 'bold',...
+                'Position', [0.57 0.38 0.02 0.035]);
+            
+            edWdz = uicontrol('Style', 'edit',...
+                'String', '3',...
+                'Units', 'normalized',...
+                'Callback', @changeEdit ,...
+                'HorizontalAlignment', 'left',...
+                'FontWeight', 'bold',...
+                'Position', [0.6 0.35 0.02 0.035]);
+            
+            uicontrol('Style', 'text',...
+                'String', 'WDZ: ',...
+                'Units', 'normalized',...
+                'HorizontalAlignment', 'left',...
+                'FontWeight', 'bold',...
+                'Position', [0.57 0.35 0.02 0.035]);
+            
+            % List box
+            listMethod = uicontrol('Style', 'listbox', ...
+                'String', {'arPLS'},...
+                'Value', 1,...
+                'Units', 'normalized',...
+                'Tag', 'listMethod',...
+                'Enable', 'off',...
+                'Position', [0.8 0.7 0.16 0.25]);
+            
+            % Edit box
+            editPara1 = uicontrol('Style', 'edit', ...
+                'String', '1E6',...
+                'Units', 'normalized',...
+                'Callback', @changeEdit ,...
+                'Tag', 'editPara1',...
+                'Position', [0.8 0.63 0.05 0.03]);
+            
+            uicontrol('Style', 'text',...
+                'String', 'smoothness (0 - 1E15)',...
+                'Units', 'normalized',...
+                'HorizontalAlignment', 'left',...
+                'FontWeight', 'bold',...
+                'Position', [0.86 0.63, 0.1, 0.03]);
+            
+            % Push buttions
+            
+            uicontrol('Style', 'pushbutton', ...
+                'String', 'Next profile',...
+                'Units', 'normalized',...
+                'Callback', @newProfile,...
+                'Position', [0.79 0.37 0.06 0.044]);
+            
+            uicontrol('Style', 'pushbutton', ...
+                'String', 'Done',...
+                'Units', 'normalized',...
+                'Callback', @done,...
+                'Position', [0.89 0.37 0.06 0.044]);
+            
+            yy = getProfile(obj);
+            parametersBaseline = doBaselineCorrection(yy);
+            
+            function newProfile(~, ~)
+                yy = getProfile(obj);
+                parametersBaseline = doBaselineCorrection(yy);
+            end
+            
+            function done(~, ~)
+                parametersBaseline.obj = obj;
+                parametersBaseline.noise =  str2double(edNoise.String);
+                parametersBaseline.wdz = str2double(edWdz.String);
+                assignin('base', 'par4bas', parametersBaseline)
+                delete(gcf)
+            end
+            
+            function parametersBaseline = changeEdit(~, ~)
+                parametersBaseline = doBaselineCorrection(yy);
+            end
+            
+            function currentProfile = getProfile(obj)
+                nbrProf = sum(obj.IndMax);
+                lengthTm = length(obj.TimeAxe.Data);
+                tgtProf = round(rand*(nbrProf-1))+1;
+                fidRead = fopen(obj.Link2SelPrf, 'rb');
+                fseek(fidRead, (tgtProf-1)*4, 'bof');
+                currentProfile(:,1) = obj.TimeAxe.Data;
+                currentProfile(:,2) = ...
+                    fread(fidRead, lengthTm, obj.Precision, (nbrProf-1)*4);
+                fclose(fidRead);
+            end
+            
+            function parametersBaseline = doBaselineCorrection(yy)
+                value = get(listMethod, 'Value');
+                switch value
+                    case 1
+                        parameter(1) = str2double(get(editPara1, 'String'));
+                        notZeros = find(yy(:,2) ~= 0);
+                        [baseline, ~, parameter] = doArPLS(yy(notZeros,2),...
+                            parameter(1));
+                        set(editPara1, 'String', parameter(1));
+                        parametersBaseline.type = 'arPLS';
+                        parametersBaseline.parameter(1) = str2double(get(editPara1, 'String'));
+                        
+                end
+                
+                
+                axes(ax1)
+                plot(yy(:,2), 'k'), hold on, plot(notZeros, baseline, 'r'), hold off;
+                
+                bsl = zeros(length(yy(:,2)), 1);
+                bsl(notZeros) = round(baseline);
+                filteredyy = yy(:,2) - bsl;
+                noise = str2double(edNoise.String);
+                wdz = str2double(edWdz.String);
+                mat4noise = zeros(length(filteredyy), 2*wdz+1);
+                for ii = 1:2*wdz+1
+                    id1 = max(1, ii-wdz);
+                    id2 = max(2*wdz-ii, 1);
+                    mat4noise(id1:end-id2+1, ii) = filteredyy(id2:end-id1+1);
+                end
+                w = max(mat4noise, [], 2) < 3*noise;
+                filteredyy(w) = 0;
+                indNeg = filteredyy <= 0;
+                filteredyy(indNeg) = 0;
+                
+                axes(ax2)
+                plot(filteredyy, 'k');
+                
+            end
+        
+
+
+set(f, 'Visible', 'on');
+        end
+        
+    end
+    
+end
+
